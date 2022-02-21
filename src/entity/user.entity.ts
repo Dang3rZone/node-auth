6bd89c94b0c35013107755b2677daf0f3ed60842
Andres Rosales <andresrosalespinzon@gmail.com>
2022-02-21 19:45:03 +0100

    feat: user entity

---
+++ src/entity/user.entity.ts
@@ -0,0 +1,19 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    password!: string;
+
+    @Column({unique: true})
+    email!: string;
+
+    @Column()
+    firstName!: string;
+
+    @Column()
+    lastName!: string;
+}
