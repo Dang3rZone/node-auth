b226b0a51b636a1b06fb5149bab3608e6776c3ce
Andres Rosales <andresrosalespinzon@gmail.com>
2022-02-21 17:15:26 +0100

    feat: import express and setup port

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Worlds!');
+});
+
+app.listen(8000, () => {
+  console.log('Server started on port 8000');
+});
