abb4b0abc2586fa20d8eea436d7be6f7712331d0
Andres Rosales <andresrosalespinzon@gmail.com>
2022-02-22 11:02:24 +0100

    feat: added access and refresh token

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import {getRepository} from "typeorm";
 import {User} from "../entity/user.entity";
 import bcryptjs from "bcryptjs";
+import {sign} from "jsonwebtoken";
 
 export const Register = async (req: Request, res: Response) => {
     const body = req.body;
@@ -46,5 +47,23 @@ export const Login = async (req: Request, res: Response) => {
         });
     }
 
-    res.send(user);
+    const accessToken = sign({
+        id: user.id,
+    }, "access_secret", {expiresIn: "30s"});
+
+    res.cookie("access_token", accessToken, {
+        httpOnly: true,
+        maxAge: 1000 * 24 * 60 * 60,
+    });
+
+    const refreshToken = sign({
+        id: user.id,
+    }, "refresh_token", {expiresIn: "1w"});
+
+    res.cookie("refresh_token", refreshToken, {
+        httpOnly: true,
+        maxAge: 1000 * 24 * 60 * 60 * 7,
+    });
+
+    res.send({message: "Logged in"});
 };
