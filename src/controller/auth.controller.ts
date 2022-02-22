a90a2c06df4e5eaf565c9bf9a035123fc7b949e1
Andres Rosales <andresrosalespinzon@gmail.com>
2022-02-22 09:38:22 +0100

    feat: added login route

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,24 +1,50 @@
 import {Request, Response} from "express";
 import {getRepository} from "typeorm";
 import {User} from "../entity/user.entity";
+import bcryptjs from "bcryptjs";
 
 export const Register = async (req: Request, res: Response) => {
     const body = req.body;
 
     if (body.password !== body.confirmPassword) {
-        return res.status(400).json({
-            ok: false,
+        return res.status(400).send({
             message: "Passwords do not match"
         });
     }
 
     const user = await getRepository(User).save({
+        password: await bcryptjs.hash(body.password, 12),
+        email: body.email,
         firstName: body.firstName,
         lastName: body.lastName,
-        email: body.email,
-        password: body.password
     });
 
-    res.send(body);
+    res.send(user);
 
+};
+
+export const Login = async (req: Request, res: Response) => {
+    const body = req.body;
+
+    const user = await getRepository(User).findOne({
+        where: {
+            email: body.email
+        }
+    });
+
+    if (!user) {
+        return res.status(400).send({
+            message: "User not found"
+        });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(body.password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).send({
+            message: "Invalid password"
+        });
+    }
+
+    res.send(user);
 };
