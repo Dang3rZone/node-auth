5a47a82a247e2013face31b6105d7ade588c7f57
Andres Rosales <andresrosalespinzon@gmail.com>
2022-02-22 11:06:57 +0100

    fix: added cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import {createConnection} from 'typeorm';
 import {routes} from "./routes";
+import cors from 'cors';
 
 createConnection().then(() => {
 
     const app = express();
 
     app.use(express.json());
+    app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+        credentials: true
+    }));
 
     routes(app);
 
