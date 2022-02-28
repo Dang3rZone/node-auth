5e1f9c88a989bfbfbf35f6d1474d53d32b49e0b5
Andres Rosales <andresrosalespinzon@gmail.com>
2022-02-28 11:08:10 +0100

    feat: added nodemailer and reset password

--- src/controller/forgot.controller.ts
+++ src/controller/forgot.controller.ts
@@ -1,14 +1,33 @@
 import {Request, Response} from "express";
 import {getRepository} from "typeorm";
 import {Reset} from "../entity/reset.entity";
+import {createTransport} from "nodemailer";
 
 export const Forgot = async (req: Request, res: Response) => {
+
     const {email} = req.body;
     const token = Math.random().toString(36).substring(2, 12);
 
-    const reset = await getRepository(Reset).save({
+    await getRepository(Reset).save({
         email,
         token,
     });
-    res.send(reset);
+
+    const transporter = createTransport({
+        host: '0.0.0.0',
+        port: 1025,
+    });
+
+    const url = `http://localhost:3000/reset/${token}`;
+
+    await transporter.sendMail({
+        from: 'from@example.com',
+        to: email,
+        subject: 'Reset Password',
+        html: `Click <a href="${url}">here</a> to reset your password.`,
+    })
+
+    res.send({
+        message: 'Please check your email for a link to reset your password.',
+    });
 };
