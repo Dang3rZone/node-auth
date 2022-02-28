0d25e1c5d917d685526edef6cc5462496d9d420b
Andres Rosales <andresrosalespinzon@gmail.com>
2022-02-28 19:11:55 +0100

    feat: added routes and links

--- Front-end/src/App.svelte
+++ Front-end/src/App.svelte
@@ -1,9 +1,31 @@
 <script>
 	import Login from "./pages/Login.svelte";
     import Register from "./pages/Register.svelte";
+    import Home from "./pages/Home.svelte";
+    import Router, {link} from "svelte-spa-router";
 
+    const routes = {
+        "/": Home,
+        "/login": Login,
+        "/register": Register
+    };
 
 </script>
 
-<Login />
-<Register />
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/" use:link class="nav-link px-2 text-white">Home</a></li>
+            </ul>
+
+            <div class="text-end">
+                <a href="/login" use:link type="button" class="btn btn-outline-light me-2">Login</a>
+                <a href="/register" use:link type="button" class="btn btn-warning">Sign-up</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<Router {routes}/>
--- Front-end/src/pages/Register.svelte
+++ Front-end/src/pages/Register.svelte
@@ -1,28 +1,46 @@
+<script>
+    import axios from "axios";
+    import push from "svelte-spa-router";
+
+    let first_name='';
+    let last_name='';
+    let email='';
+    let password='';
+    let confirm_password='';
+
+    $: submit = async()=> {
+        await axios.post('http://localhost:8000/api/v1/users/register', {first_name, last_name, email, password, confirm_password});
+
+        await push('/login');
+    }
+</script>
+
 <main class="form-signin">
-    <form>
+    <form on:submit|preventDefault={{submit}}>
         <h1 class="h3 mb-3 fw-normal">Please register</h1>
+
         <div class="form-floating">
-            <input  class="form-control"  placeholder="First name">
+            <input bind:value={first_name} class="form-control"  placeholder="First name">
             <label >First name</label>
         </div>
 
         <div class="form-floating">
-            <input  class="form-control"  placeholder="Last name">
+            <input bind:value={last_name}   class="form-control"  placeholder="Last name">
             <label >Last name</label>
         </div>
 
         <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input bind:value={email} type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
             <label >Email address</label>
         </div>
 
         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input bind:value={password}  type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Password</label>
         </div>
 
         <div class="form-floating">
-            <input type="password" class="form-control" placeholder="Password Confirm">
+            <input bind:value={confirm_password}  type="password" class="form-control" placeholder="Password Confirm">
             <label for="floatingPassword">Password confirm</label>
         </div>
         <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
