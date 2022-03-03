7c7f2b3aa2cd9fe06984a09313aa63135579b4ac
Andres Rosales <andresrosalespinzon@gmail.com>
2022-03-03 11:36:14 +0100

    fix: changed axios version and fixed api url

--- Front-end/src/pages/Register.svelte
+++ Front-end/src/pages/Register.svelte
@@ -1,22 +1,23 @@
 <script>
     import axios from "axios";
-    import push from "svelte-spa-router";
+    import {push} from "svelte-spa-router";
 
     let first_name='';
     let last_name='';
     let email='';
     let password='';
-    let confirm_password='';
+    let password_confirm='';
 
-    $: submit = async()=> {
-        await axios.post('http://localhost:8000/api/v1/users/register', {first_name, last_name, email, password, confirm_password});
+    $: submit = async ()=> {
+        await axios.post('http://localhost:8000/api/register', {
+            first_name, last_name, email, password, password_confirm});
 
         await push('/login');
     }
 </script>
 
 <main class="form-signin">
-    <form on:submit|preventDefault={{submit}}>
+    <form on:submit|preventDefault={submit}>
         <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
         <div class="form-floating">
@@ -40,10 +41,10 @@
         </div>
 
         <div class="form-floating">
-            <input bind:value={confirm_password}  type="password" class="form-control" placeholder="Password Confirm">
+            <input bind:value={password_confirm}  type="password" class="form-control" placeholder="Password Confirm">
             <label for="floatingPassword">Password confirm</label>
         </div>
         <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
     </form>
 </main>
