03c1bd5b9aada65b0d398b99e4ab25186a28125f
Andres Rosales <andresrosalespinzon@gmail.com>
2022-03-03 14:22:15 +0100

    feat: added log in in the front

--- Front-end/src/pages/Login.svelte
+++ Front-end/src/pages/Login.svelte
@@ -1,15 +1,38 @@
+<script>
+    import axios from "axios";
+    import {push} from "svelte-spa-router";
+
+    let email = '';
+    let password = '';
+
+    $: submit = async () => {
+        const {data} = await axios.post('http://localhost:8000/api/login', {
+            email, password
+        }, {withCredentials: true});
+
+        axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+        
+        await push('/');
+    }
+</script>
+
 <main class="form-signin">
-    <form>
+    <form on:submit|preventDefault={submit}>
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
         <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+            <input bind:value={email} type="email" class="form-control" id="floatingInput"
+                   placeholder="name@example.com">
+            <label>Email address</label>
         </div>
+
         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input bind:value={password} type="password" class="form-control" id="floatingPassword"
+                   placeholder="Password">
             <label for="floatingPassword">Password</label>
         </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
 
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
     </form>
 </main>
